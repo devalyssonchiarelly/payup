232ea2ac688e65b5de8c99b1a6b73f963b901ed2
Alysson Chiarelly <alyssonclr@gmail.com>
2026-06-14 11:21:39 -0300

    Finishing initial setup

---
+++ src/app/index.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Index</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontFamily: 'K2D',
+    fontWeight: '700',
+    fontSize: 70,
+  },
+});
